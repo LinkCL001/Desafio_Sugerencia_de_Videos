55970b969974529612ad1c11caa1586ab6bd8c91
JuanQuezada <ju.quezada.c@gmail.com>
2021-10-01 21:31:13 -0300

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
+//1. Implementar el Patrón Módulo mediante IIFE, en donde:
+// ● Se cree una función privada que reciba la url del video y el id de la etiqueta
+// iframe, para así poder mostrar los videos en el documento HTML. Dato:
+// puedes utilizar la instrucción “setAttribute” para manipular el DOM.
+// ● Se retorne una función pública que reciba los parámetros (url, id), y realice el
+// llamado a la función interna (privada) para insertar los elementos recibidos.
+
 const setIframeUrl = (() =>{
     const inyectaDato = (url, id) => {
         document.querySelector(`#${id}`).setAttribute('src', url);
     }
     return (url, id) => inyectaDato(url, id);
 })();
+
+// 2. Establecer una clase padre denominada Multimedia para:
+// ● Recibir la propiedad url, la cual será el atributo
+// src que necesite la etiqueta iframe para poder mostrar el video.
+// ● Proteger el atributo de la clase implementado closures.
 
 class Multimedia { //Crear una clase padre “Multimedia”,
     constructor(url) {
@@ -15,77 +27,61 @@ class Multimedia { //Crear una clase padre “Multimedia”,
     get url () {
         return this.getUrl(); 
     }
+// ● Agregar un método denominado “setInicio”, que retorne el siguiente mensaje:
+// “Este método es para realizar un cambio en la URL del video”.
     setInicio() {
         return "Este método es para realizar un cambio en la URL del video”."
     }
 };
-
-class Reproductor extends Multimedia { //Crear una clase “Reproductor”, siendo hija de la clase padre Multimedia
-    constructor (url,id){
-        super(url)
-        let _id = id;
-        this.getId = () => _id;
-        // this.setId = (nueva_id) => _id = nueva_id;
-    }
-    get id(){
-        return this.getId();
-    }
-    // set id(nueva_id){
-    //     this.setId(nueva_id);
-    // }
-    playMultimedia(){
-        setIframeUrl(this.url, this.id);
-    }
-    setInicio(){
-        
-    }
-}
-let musica = new Reproductor('https://www.youtube.com/embed/TOR4Gm7QIw8',"musica");
-let peliculas = new Reproductor('https://www.youtube.com/embed/5PSNL1qE6VY',"peliculas")
-let series = new Reproductor('https://www.youtube.com/embed/nEGQbVacOaU',"series")
-musica.playMultimedia();
-peliculas.playMultimedia();
-series.playMultimedia();
-
-
-//1. Implementar el Patrón Módulo mediante IIFE, en donde:
-// ● Se cree una función privada que reciba la url del video y el id de la etiqueta
-// iframe, para así poder mostrar los videos en el documento HTML. Dato:
-// puedes utilizar la instrucción “setAttribute” para manipular el DOM.
-// ● Se retorne una función pública que reciba los parámetros (url, id), y realice el
-// llamado a la función interna (privada) para insertar los elementos recibidos.
-
-// 2. Establecer una clase padre denominada Multimedia para:
-// ● Recibir la propiedad url, ejemplo:
-// “https://www.youtube.com/embed/5PSNL1qE6VY”, la cual será el atributo
-// src que necesite la etiqueta iframe para poder mostrar el video.
-// ● Proteger el atributo de la clase implementado closures.
-// ● Agregar un método denominado “setInicio”, que retorne el siguiente mensaje:
-// “Este método es para realizar un cambio en la URL del video”.
 
 // 3. Crear una clase “Reproductor”, siendo hija de la clase padre Multimedia para:
 // ● Recibir la propiedad id, la cual será el elemento del DOM que se necesita para
 // poder agregar la URL en la etiqueta iframe que corresponda. Por ejemplo: Si
 // se envía una URL para Música, el id debe ser el perteneciente a la etiqueta
 // iframe que se encuentra en la sección de música.
-// _ 3
-// www.desafiolatam.com
+
+class Reproductor extends Multimedia { //Crear una clase “Reproductor”, siendo hija de la clase padre Multimedia
+    constructor (url,id){
+        super(url)
+        let _id = id;
+        this.getId = () => _id;
+    };
+    get id(){
+        return this.getId();
+    }
 // ● Crear un método denominado “playMultimedia”, que permita hacer el llamado
 // a la función pública de la IIFE, enviando los atributos url y id.
+    playMultimedia(){
+        setIframeUrl(this.url, this.id);
+    };
 // ● Agregar un método denominado “setInicio”, que reciba y agregue un tiempo
 // de inicio a la URL de la etiqueta iframe. Se puede utilizar el método
 // “setAttribute” para modificar la URL agregando al final de la misma lo
 // siguiente: “?start=${tiempo}”. Esto permitirá que cualquiera de los videos que
 // implemente el método inicie en el tiempo pasado como argumento al método
 // al ser invocado.
-
+    setInicio(){
+        //?start=${2044}
+    }
+}
 // 4. Instanciar la clase hija pasando como argumento la URL y el id para cada etiqueta
 // iframe, por lo que se deben crear tres instancias, una para música, otra para película
 // y otra para serie, con sus respectivas URL.
 
+let musica = new Reproductor('https://www.youtube.com/embed/TOR4Gm7QIw8',"musica");
+let peliculas = new Reproductor('https://www.youtube.com/embed/5PSNL1qE6VY',"peliculas")
+let series = new Reproductor('https://www.youtube.com/embed/nEGQbVacOaU',"series")
+
 // 5. Invocar al método “playMultimedia” para cada instancia creada, mostrando así los
 // videos en el documento HTML.
 
+musica.playMultimedia();
+peliculas.playMultimedia();
+series.playMultimedia();
+
 // 6. Utiliza el método “setInicio” para modificar el tiempo de inicio en alguna de las
 // instancias creadas.
-//
+
+musica.setInicio();
+peliculas.setInicio();
+series.setInicio();
